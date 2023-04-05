17ddc6d0570446f9ae72d7f75a5143ef4fb19124
FajryAriansyah <fajry@mhs.unsyiah.ac.id>
2023-04-05 09:19:56 +0700

    Membuat Tampilan View

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,15 @@ use App\Http\Controllers\SolveblemController;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Hered is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
 */
 
+Route::get('/', function () {
+    return view('landing');
+});
 
 Route::get('solveblem/submissions', [SolveblemController::class, 'index']);
 
@@ -28,3 +31,15 @@ Route::get('/', function () {
 });
 
 Route::get('solveblem/user/contest', [SolveblemController::class, 'user_contest']);
+
+Route::get('/user-create-contest', function () {
+    return view('user-create-contest');
+});
+
+Route::get('/problem', function() {
+    return view('problem');
+});
+Route::get('/create-problem', function() {
+    return view('create-problem');
+});
+
