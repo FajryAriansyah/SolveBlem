d2c2dcbec1570e61740449a7496a98eb54e7458f
FajryAriansyah <fajry@mhs.unsyiah.ac.id>
2023-04-05 09:19:56 +0700

    Membuat Tampilan View

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,25 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Hered is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
+
+
+Route::get('/user-create-contest', function () {
+    return view('user-create-contest');
+});
+
+Route::get('/problem', function() {
+    return view('problem');
+});
+Route::get('/create-problem', function() {
+    return view('create-problem');
+});
+
