f112524349c05cf4e8084d835517d6ef8f6a3a84
FajryAriansyah <fajry@mhs.unsyiah.ac.id>
2023-05-14 17:00:50 +0700

    Menambahkan backend contest

--- app/Http/Controllers/ContestController.php
+++ app/Http/Controllers/ContestController.php
@@ -13,7 +13,18 @@ class ContestController extends Controller
     public function create(){
         return view('contest.createContest');
     }
-    public function store(){
+    public function store(Request $request){
+
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'user_id' => 'required',
+            'start_at' => 'required',
+            'duration' => 'required|',
+            'max_participant' => 'required',
+            'password' => 'required',
+        ]);
+
+        Contest::create[$validated];
 
         return redirect('contest/{}');
     }
--- resources/views/contest/createContest.blade.php
+++ resources/views/contest/createContest.blade.php
@@ -2,7 +2,7 @@
 @section('container')
   <div class="content">
     <h1 class="font-bold text-[28px] underline" >Contest</h1>
-    <div class="form" action="/contest/create" method="POST">
+    <form class="form" action="/contest/store" method="POST">
         <div class="inputForm">Contest Name : <input name="title" type="text" placeholder="Contest Name"></div><br>
         <div class="inputForm">Authors Name : <input name="user_id" type="text" placeholder="Authors Name"></div><br>
         <div class="inputForm">Start At : <input name="start_at" type="text" placeholder="Start At"></div><br>
@@ -10,23 +10,23 @@
         <div class="inputForm">Max Participant : <input name="max_participant" type="text" placeholder="Max Participant"></div><br>
         <div class="inputForm">Password : <input name="password" type="text" placeholder="Password"></div><br>
         <br>
-        Problems
+        {{-- Problems
         <br><br>
         <div class="row gap-3">
-          
+           --}}
           {{-- @foreach ($problems as $problem)
           <div class="card col-md-4">
             <span>{{ $problem->judul }}</span>
           </div>
           @endforeach --}}
-          <div class="card col-md-4">
+          {{-- <div class="card col-md-4">
               <span onclick="window.location='{{ url("/problem/create") }}'">Create a Problem</span>
           </div>
-        </div>
+        </div> --}}
         <br><br>
         <button type="submit" name="submit" class="bg-blue-800 px-5 py-2 rounded-lg border-1 border-black hover:bg-violet-600 text-white">
         Buat Contest
       </button><br><br>
-    </div>
+    </form>
 </div>
 @endsection
